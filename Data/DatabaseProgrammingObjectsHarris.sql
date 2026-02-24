ff10d2646dd0fe40a4df6f5beeb8ad63a816e21f
Trent Harris <tdh00010@mix.wvu.edu>
2026-02-24 12:45:54 -0500

    Added Query

--- Data/DatabaseProgrammingObjectsHarris.sql
+++ Data/DatabaseProgrammingObjectsHarris.sql
@@ -1,9 +1,9 @@
 -- 3 queries
 -- 1 each for ConferenceDivision and team tables, and 1 join query
 USE MIST353_NFL_RDB_Harris;
-GO
+go
 
---Query for ConferenceDivision table
+/*--Query for ConferenceDivision table
 SELECT
     ConferenceDivisionID,
     Conference,
@@ -27,4 +27,29 @@ FROM Team AS t
 INNER JOIN ConferenceDivision AS cd
     ON t.ConferenceDivision = cd.ConferenceDivisionID
 ORDER BY t.TeamName;
+GO*/
+
+--4th query
+
+Create or alter procedure procGetTeamsByConferenceDivision
+(
+    @Conference NVARCHAR(50) = null,
+    @Division NVARCHAR(50) = null
+)
+AS
+Begin
+    SELECT T.TeamName, T.TeamColor, CD.Conference, CD.Division
+    FROM Team AS T
+    INNER JOIN ConferenceDivision AS CD
+        ON T.ConferenceDivision = CD.ConferenceDivisionID
+    WHERE (@Conference IS NULL OR CD.Conference =IsNull(@ConferenceName, Conference))
+      AND (@Division IS NULL OR CD.Division = IsNull(@DivisionName, Division))
+    ORDER BY T.TeamName;
+End;
+/*
+execute ProcGetTeamsByConferenceDivision
+@ConfernceName = 'AFC'
+@DivisionName = 'North'
+
+*/
 GO
