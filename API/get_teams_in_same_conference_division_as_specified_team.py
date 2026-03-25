288c8531139541192fb1bbf5767b8419f00ed804
Trent Harris <tdh00010@mix.wvu.edu>
2026-03-25 15:32:58 -0400

    api

--- API/get_teams_in_same_conference_division_as_specified_team.py
+++ API/get_teams_in_same_conference_division_as_specified_team.py
@@ -1,29 +1,25 @@
 from get_db_connection import get_db_connection
-
-def get_teams_by_conference_division(
-    conference: str = None,
-    division: str = None
+def get_teams_in_same_conference_division_as_specified_team(
+    team_name: str
 ):
     conn = get_db_connection()
     cursor = conn.cursor()
 
     cursor.execute(
-        "{call procGetTeamsByConferenceDivision(?, ?)}",
-        (conference, division)
+        "{call procGetTeamsInSameConferenceDivisionAsSpecifiedTeam(?)}",
+        (team_name,)
     )
 
     rows = cursor.fetchall()
     conn.close()
-
+    
     results = [
         {
             "TeamName": row.TeamName,
             "Conference": row.Conference,
             "Division": row.Division,
-            "TeamColor": row.TeamColor
-        }
-        for row in rows
-        ]
-    return {"data": results}
+            }
+            for row in rows
+    ]
 
-    
+    return {"data": results}
--- API/nfl_playoffs_api.py
+++ API/nfl_playoffs_api.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from get_teams_by_conference_division import get_teams_by_conference_division
-
+from get_teams_in_same_conference_division_as_specified_team import get_teams_in_same_conference_division_as_specified_team
 app = FastAPI()
 
-@app.get("/teams")
-def read_teams(conference: str = None, division: str = None):
+@app.get("/get_teams_by_conference_division")
+def get_teams_by_conference_division_api(conference: str = None, division: str = None):
     return get_teams_by_conference_division(conference, division)
 
-@app.get("/")
-def home():
-    return {"message": "NFL Playoffs API is running"}
+@app.get("/get_teams_in_same_conference_division_as_specified_team")
+def get_teams_in_same_conference_division_as_specified_team_api(team_name: str):
+    return get_teams_in_same_conference_division_as_specified_team(team_name)
