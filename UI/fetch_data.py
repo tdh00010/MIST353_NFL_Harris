c3759606b4708b631e3a994b90882891f353bef8
Trent Harris <tdh00010@mix.wvu.edu>
2026-04-21 00:10:07 -0400

    fetch data update

--- UI/fetch_data.py
+++ UI/fetch_data.py
@@ -2,7 +2,8 @@ import streamlit as st
 import requests
 import pandas as pd
 
-FASTAPI_URL = "http://localhost:8000"
+#FASTAPI_URL = "http://localhost:8000"
+FASTAPI_URL = "https://mist353-api-harris.azurewebsites.net/"
 
 def fetch_data(endpoint: str, input_params: dict, method: str = "GET"):
     if method == "GET":
